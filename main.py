1a79bd34c4b403a4bb5eb15fa237122c90595802
TacoFury <108041377+TacoFury@users.noreply.github.com>
2022-06-22 21:04:59 -0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# Author: Francisco Martinez
-# GitHub username: TacoFury
-# Date: 06/22/2022
-# Description: Asks the user for their favorite animal and then
-#              prints out that that is their favorite animal
-
-fave_animal = input("Please enter your favorite animal: ")
-print("Your favorite animal is the " + fave_animal + ".")
